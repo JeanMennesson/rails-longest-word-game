ec72b7dd10168f8965ed8508bb7082ebc2b14b7e
Jean Mennesson <mennessonj@gmail.com>
2018-11-26 17:47:38 +0100

    In progress

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,11 +8,25 @@ class GamesController < ApplicationController
       new_letter = ('a'..'z').to_a.sample
       @letters << new_letter
     end
-
   end
 
   def score
-    raise
-    binding.pry
+    @attempt = params[:word]
+    @valid = @attempt.split(//).all? do |letter|
+      @attempt.delete_at(@attempt.index(letter)) if @attempt.include?(letter)
+    end
   end
+
+  def english_word(attempt)
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+    english_word_serialized = open(url).read
+    english_word_check = JSON.parse(english_word_serialized)
+    english_word_check['found']
+  end
+
+  def run_games(attempt)
+
+  end
+
+
 end
